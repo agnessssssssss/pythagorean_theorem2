35606f496262f139fdbd8ce0937486e1863de0ba
Agnes Aus <agnes.aus@tptlive.ee>
2024-02-23 17:55:26 +0200

    pythagoras

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ nunjucks.configure('views', {
 app.use(express.urlencoded());
 
 function calculateHypotenuse(a, b) {
-    return Math.sqrt(a**2 + b**2);
-  }
+  return Math.sqrt(a * a + b * b);
+}
 
 app.get('/pythagorean_theorem', (req, res) => {
     res.render('pythagorean_theorem.njk');
   });
 
-  app.post('/a', function (req,res) {
+  app.post('/pythagorean_theorem', function (req, res) {
     let a = req.body.a;
     let b = req.body.b;
-    let c = calcHypotenuse(a, b);
-    res.render('answer.njk', {a, b, c});
+    let c = calculateHypotenuse(a, b);
+    res.render('pythagorean_theorem.njk', {a, b, c});
   });
 
   app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
+    console.log(`Example app listening on port http://localhost:${port}`);
 
   });
